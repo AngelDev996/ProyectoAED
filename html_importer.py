336e4164b989b98b0dd4cd5bce2d29192c60fbed
AngelV <arivalladares2.0@gmail.com>
2021-04-22 15:49:52 -0600

    update

--- html_importer.py
+++ html_importer.py
@@ -1,13 +1,14 @@
 import urllib.request
 
+#Clase que se encarga con obtener el codigo fuente de un witio web
 class Html_import:
 
-    def __init__(self):
-        self.url = " "
+    def __init__(self): #Metodo init que inicializa la variable "url" que es la que usaremos para obtener la pagina web
+        self.url = " " 
         
-    def obtener_html(self):
+    def obtener_html(self): #Metodo que obtiene el codigo fuente en html de la pagina solitada
         self.webUrl = urllib.request.urlopen(self.url)
         self.data = self.webUrl.read()
     
-    def imprimir_html(self):
+    def imprimir_html(self): #Metodo que imprime el codigo fuente de la pagina solicitada
         print(self.data)
